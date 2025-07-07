d722687fe0bb74e715a6ad2ac49bae46ec915ff2
Trisha Karmakar <139961048+trishakarma@users.noreply.github.com>
2025-07-08 01:43:51 +0530

    closest pitch

--- audio-effects.py
+++ audio-effects.py
@@ -3,3 +3,9 @@
 import pyaudio
 import numpy as np 
 import librosa
+
+def closest_pitch(f0):
+    midi_note = np.around(librosa.hz_to_midi(f0))
+    nan_indices = np.isnan(f0)
+    midi_note[nan_indices] = np.nan
+    return librosa.midi_to_hz(midi_note)
